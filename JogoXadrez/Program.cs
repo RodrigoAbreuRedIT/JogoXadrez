080c4e6936d020ba27fe9090eb0b71fdaeb44831
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-11-26 17:36:46 +0000

    Criando a classe Posicao

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace JogoXadrez {
     class Program {
         static void Main(string[] args) {
-            
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
