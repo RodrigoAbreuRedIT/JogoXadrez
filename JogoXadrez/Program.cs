c141959cd6aa9c08b3533dada007f751ead392ea
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-11-26 17:47:42 +0000

    Comecando a implementar o tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace JogoXadrez {
     class Program {
         static void Main(string[] args) {
-            Posicao p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
